07e1522c108e4c8cd608a23b76387d52cfee48c0
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-04-19 18:40:16 +0300

    add styles for Caption component

--- src/components/Caption/Caption.tsx
+++ src/components/Caption/Caption.tsx
@@ -1,10 +1,12 @@
-import React, { FC, HTMLAttributes, memo } from 'react';
+import React, { FC, HTMLAttributes, useMemo, memo } from 'react';
+import cn from 'classnames';
 
 interface CaptionProps extends HTMLAttributes<HTMLSpanElement> { }
 
-const Caption: FC<CaptionProps> = memo((props: CaptionProps) => {
-    console.log(123);
-    return <span {...props} />;
+const Caption: FC<CaptionProps> = memo(({ className, ...restProps }: CaptionProps) => {
+    const classNames = useMemo(() => cn(className, 'Caption'), [className]);
+
+    return <span className={classNames} {...restProps} />;
 });
 
 export default Caption;
