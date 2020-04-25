b0e48302dac6a552f2569eecdf431f6ccc4851a8
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-04-25 20:53:26 +0300

    add imports Headline, Input & Textarea components

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ import './styles/index.css';
 
 export { default as Caption } from './components/Caption';
 export { default as Footnote } from './components/Footnote';
+export { default as Headline } from './components/Headline';
+export { default as Input } from './components/Input';
+export { default as Textarea } from './components/Textarea';
