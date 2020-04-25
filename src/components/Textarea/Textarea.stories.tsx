7cf1c3ed940b80d36b830c6c18b0793f0f960732
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-04-25 18:05:01 +0300

    add Textarea component

---
+++ src/components/Textarea/Textarea.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Textarea from './Textarea';
+import { text, number, boolean } from '@storybook/addon-knobs/';
+
+storiesOf('Textarea', module)
+    .add(
+        'default',
+        () => {
+            const maxLength = number('maxLength', 20);
+
+            return (
+                <Textarea
+                    placeholder={text('placeholder', 'Enter text...')}
+                    label={text('label', 'Caption')}
+                    hint={text('hint', 'Footnote')}
+                    error={boolean('error', false)}
+                    maxLength={maxLength} />
+            );
+        },
+        {
+            info: { inline: true },
+            text: `
+            ### Notes
+
+            Simple Textarea component
+
+            ### Usage
+            ~~~ts
+            <Textarea>Some text</Textarea>
+            ~~~
+            `
+        }
+    );
---
+++ src/components/Textarea/Textarea.tsx
@@ -0,0 +1,60 @@
+import React, {
+    FC, InputHTMLAttributes, ChangeEvent,
+    useRef, useCallback, useLayoutEffect,
+    memo, forwardRef
+} from 'react';
+import cn from 'classnames';
+
+import Field, { FieldProps } from '../Field';
+
+interface TextareaProps extends InputHTMLAttributes<HTMLTextAreaElement>, FieldProps { }
+
+export function changeHeight(element: HTMLTextAreaElement | ChangeEvent<HTMLTextAreaElement>, defaultHeight = 20) {
+    if (element) {
+        const target = (element instanceof HTMLTextAreaElement) ? element : element.target;
+        target.style.height = `${defaultHeight}px`;
+        target.style.height = `${target.scrollHeight}px`;
+    }
+}
+
+const Textarea: FC<TextareaProps> = memo(forwardRef(({
+    className,
+    view, label, value, maxLength, error, hint, autofocus, aside,
+    onChange, triggerMaxLength,
+    ...restProps
+}: TextareaProps, ref) => {
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+    const handleChange = useCallback((e: ChangeEvent<HTMLTextAreaElement>) => {
+        if (onChange) {
+            onChange(e);
+        }
+
+        changeHeight(e);
+    }, [onChange]);
+
+    useLayoutEffect(() => {
+        if (textareaRef.current) {
+            changeHeight(textareaRef.current);
+        }
+    }, [textareaRef]);
+
+    return (
+        <Field
+            className={cn(className, 'Textarea')}
+            view={view}
+            label={label}
+            value={value}
+            maxLength={maxLength}
+            error={error}
+            hint={hint}
+            autofocus={autofocus}
+            aside={aside}
+            triggerMaxLength={triggerMaxLength}
+            onChange={handleChange}>
+            <textarea className="reset-indent" {...restProps} ref={textareaRef} />
+        </Field>
+    );
+}));
+
+export default Textarea;
