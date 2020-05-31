8cb91aff0de651da8b41eff0f4a9f74dbb271688
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-05-31 03:13:58 +0300

    add core Label component

---
+++ src/components/core/Label.tsx
@@ -0,0 +1,40 @@
+import { css } from '../../theme';
+
+export interface CoreLabelProps {
+    priority: 'primary' | 'secondary' | 'accent' | 'accent_secondary';
+}
+
+export const CoreLabel = css<CoreLabelProps>`
+    height: 20px;
+
+    border-radius: 4px;
+
+    background-color: ${(props) => {
+        switch (props.priority) {
+            case 'secondary':
+                return props.theme.color.secondary;
+
+            case 'accent':
+                return props.theme.color.error;
+
+            case 'accent_secondary':
+                return props.theme.color.errorSecondary;
+
+            default:
+                return props.theme.color.primary;
+        }
+    }};
+    
+    color: ${(props) => {
+        switch (props.priority) {
+            case 'secondary':
+                return props.theme.color.primary;
+
+            case 'accent_secondary':
+                return props.theme.color.error;
+
+            default:
+                return props.theme.color.onPrimary.primary;
+        }
+    }};
+`;
