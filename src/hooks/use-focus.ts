c41e46d50037ec9ae7a2301e21b5e231d0b863df
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-08-02 09:33:53 +0300

    refactoring use-focus hook

--- src/hooks/use-focus.ts
+++ src/hooks/use-focus.ts
@@ -1,4 +1,4 @@
-import { FocusEventHandler, useState, useCallback } from 'react';
+import { FocusEventHandler, useState } from 'react';
 
 export interface Handlers<T> {
     onFocus: FocusEventHandler<T>;
@@ -10,8 +10,8 @@ export type UseFocus<T> = [boolean, Handlers<T>];
 export default function useFocus<T>(initialFocus = false): UseFocus<T> {
     const [focus, setFocus] = useState<boolean>(initialFocus || false);
 
-    const onFocus = useCallback<FocusEventHandler<T>>(() => setFocus(true), []);
-    const onBlur = useCallback<FocusEventHandler<T>>(() => setFocus(false), []);
+    const onFocus: FocusEventHandler<T> = () => setFocus(true);
+    const onBlur: FocusEventHandler<T> = () => setFocus(false);
 
     return [focus, { onFocus, onBlur }];
 }
