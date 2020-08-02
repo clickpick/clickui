35d31aaabb9581a0b884688a1c3525561ccb5dfe
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-08-02 09:46:47 +0300

    refactoring use-input hook

--- src/hooks/use-input.ts
+++ src/hooks/use-input.ts
@@ -1,33 +1,28 @@
-import {
-    ChangeEventHandler,
-    useState, useRef, useMemo, useCallback
-} from 'react';
+import { ChangeEventHandler, useState } from 'react';
 
-export interface UseInput<T> {
+type InputableElement = HTMLInputElement | HTMLTextAreaElement;
+
+interface Bind<T> {
     value: string;
     onChange: ChangeEventHandler<T>;
-};
+}
 
-export default function useChange<T extends HTMLElement>(defaultValue?: string | string[] | number, handleChange?: ChangeEventHandler<T>): UseInput<T> {
+type UseInput<T extends InputableElement> = [Bind<T>, string];
+
+export function useInput<T extends InputableElement>(defaultValue?: string | string[] | number, handleChange?: ChangeEventHandler<T>): UseInput<T> {
     const [stateValue, setStateValue] = useState<string>('');
-    const isControlledOutside = useRef<boolean>(defaultValue !== undefined);
-    const value = useMemo<string>(() => {
-        if (defaultValue === undefined) {
-            return stateValue;
-        }
 
-        return defaultValue.toString();
-    }, [defaultValue, stateValue]);
-
-    const onChange = useCallback((e) => {
-        if (!isControlledOutside.current) {
+    const onChange: ChangeEventHandler<T> = (e) => {
+        if (defaultValue !== undefined) {
             setStateValue(e.target.value);
         }
 
         if (handleChange) {
             handleChange(e);
         }
-    }, [isControlledOutside, handleChange]);
+    };
 
-    return { value, onChange };
+    const value = (defaultValue === undefined) ? stateValue : defaultValue.toString();
+
+    return [{ value, onChange }, value];
 }
